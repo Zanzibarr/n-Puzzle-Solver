38683400a337301c30337719fcd20b4ea071227f
Zanella Matteo <79336755+Zanzibarr@users.noreply.github.com>
2021-12-03 11:37:01 +0100

    Fixed Solver
    Minor changes in Solver checked and solved

--- Solver.java
+++ Solver.java
@@ -10,7 +10,7 @@ public class Solver {
     private static String goalBoard;
     public static int n;
 
-    public static class BoardComparator implements Comparator<Board>{
+    public static class BoardComparator implements Comparator<Board> {
         public int compare(Board b1, Board b2) {
             return b1.fCost() - b2.fCost();
         }
@@ -19,8 +19,8 @@ public class Solver {
     public static String setNullBoard() {
 
         StringBuilder strBuild = new StringBuilder();
-        
-        for (int i = 0; i < n*n; i++) {
+
+        for (int i = 0; i < n * n; i++) {
             strBuild.append(0);
             strBuild.append(" ");
         }
@@ -32,22 +32,22 @@ public class Solver {
     public static void setGoalBoard() {
 
         StringBuilder strBuild = new StringBuilder();
-        
-        for (int i = 0; i < n*n-1; i++) {
-            strBuild.append(i+1);
+
+        for (int i = 0; i < n * n - 1; i++) {
+            strBuild.append(i + 1);
             strBuild.append(" ");
         }
         strBuild.append(0);
         strBuild.append(" ");
-        
+
         goalBoard = strBuild.toString();
-        
+
     }
 
     public static void main(String[] args) throws IOException {
 
         FileReader file = new FileReader("board.txt");
-        BufferedReader fileReader = new BufferedReader(file));
+        BufferedReader fileReader = new BufferedReader(file);
 
         n = Integer.parseInt(fileReader.readLine());
         String rt = fileReader.readLine();
@@ -67,19 +67,19 @@ public class Solver {
     }
 
     public static int solve(String root) {
-        
+
         Board board = new Board(root + " ");
         board.root();
 
         PriorityQueue<Board> nextBoards = new PriorityQueue<>(new BoardComparator());
         HashMap<String, String> visited = new HashMap<>();
 
-        while(!board.toString().equals(goalBoard)) {
+        while (!board.toString().equals(goalBoard)) {
 
             Board[] children = board.children();
-            
+
             int end = children.length;
-            for (int i = 0; i < end; i++) { 
+            for (int i = 0; i < end; i++) {
 
                 if (!visited.containsKey(children[i].toString()))
                     nextBoards.add(children[i]);
@@ -91,10 +91,11 @@ public class Solver {
 
         }
 
+        visited.put(board.toString(), board.father());
         String outBuilder = board.toString();
 
         int steps = board.gCost();
-        String[] output = new String[steps+1];
+        String[] output = new String[steps + 1];
         for (int i = steps; i >= 0; i--) {
 
             output[i] = outBuilder;
@@ -102,8 +103,9 @@ public class Solver {
 
         }
 
-        for (int i = 0; i <= steps; i++) System.out.println(output[i]);
-        
+        for (int i = 0; i <= steps; i++)
+            System.out.println(output[i]);
+
         return steps;
 
     }
